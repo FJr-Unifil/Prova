69125c4486195cd19c8803de38ee6eb771b1f67b
FJr-Unifil <100450945+FJr-Unifil@users.noreply.github.com>
2022-08-26 00:09:10 -0300

    Atualização segundos

--- prova/src/prova/e3.java
+++ prova/src/prova/e3.java
@@ -4,14 +4,13 @@ import java.util.Scanner;
 
 public class e3 {
     public static void main(String[] args) {
-        // perde metade da massa a cada 50 segundos, gramas, grama = 0,5
-        // escrever massa inicial. final e tempo em horas, minutos e segundos.
         Scanner ler = new Scanner(System.in);
         int t = 0;
         double massaI;
         double massaF = 0;
         int t2 = 0;
         int t3 = 0;
+        int cont = 0;
         do{
         System.out.println("Digite a massa(em gramas)");
         massaI = ler.nextDouble();
@@ -20,12 +19,13 @@ public class e3 {
         do{
             massaF = massaF/2;
             t = t + 50;
+            cont++;
         } while (massaF > 0.5);
         if (t >= 3600){
             t2 = t/60;
             t3 = t2/60;
             t2 = t2 % 60;
-            t = t2 % 60;
+            t = t % 60;
         } else if (t <= 3599 && t > 60){
             t2 = t/60;
             t = t % 60;
@@ -33,6 +33,7 @@ public class e3 {
         System.out.println("Massa inicial: "+massaI);
         System.out.println("Massa final: "+massaF);
         System.out.println("Tempo: "+t3+" horas, "+t2+" minutos e "+t+" segundos");
+        System.out.println(cont);
 
 
     }
